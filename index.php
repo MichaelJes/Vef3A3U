365e24e4fa2f77e2eb571c64363d2726de2156ca
Michael Jes Þórsson <mikkimus@simnet.is>
2017-01-16 14:55:47 +0000

    First Update
    More tests

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
 <?php
- print("Hello world")
+ print("Hello world");
+class User
+{
+    public $email;
+    public $password;
+    public function __construct ($netfang,$lykilord) {
+        $this->username = $netfang;
+        $this->password = $lykilord;
+    }
+    public function setPassword($newpassword)
+    {
+        $this->password = $newpassword;
+    }
+    public function setEmail($newEmail)
+    {
+        $this->email = $newEmail;
+    }
+    public function getPassword()
+    {
+        return $this->password;
+    }
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+}
+$user = new User('gary@gmail.com','chicken');
+$user->getEmail()
 ?>
